dbd8bfa8cfc65da3ba000bddc711e679756a7bf3
wsuzume <joshnobus@gmail.com>
2023-11-12 17:45:26 +0900

    add remix project

---
+++ remix/project/build-environment.sh
@@ -0,0 +1,7 @@
+npm init -y
+
+# install runtime dependencies
+npm install @remix-run/node @remix-run/react @remix-run/serve isbot react react-dom
+
+# install dev dependencies
+npm install -D @remix-run/dev
